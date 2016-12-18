cf2679881147dffb0df5e3553a98c580466c6bc3
chris <544095700@qq.com>
2016-12-18 21:35:32 +0800

    update

--- js/lession.js
+++ js/lession.js
@@ -3,14 +3,19 @@ var vm = new Vue({
 	el: '#lession',
 	data: {
 		active: 1,
-		lists: ['企业高级内训', '精品课程', '线下精英特训营', '公益演讲', '名师阵容', '绎奇动态']
+		lists: ['企业高级内训', '精品课程', '公益演讲', '线下精英特训营', '名师阵容', '绎奇动态']
 	},
 	mounted: function() {
+		// 轮播
 		$('.my-slider').unslider({
 			autoplay: true,
 			arrows: false,
 			delay: 6000
 		});
+
+		// 图片晃动
+		var scene = document.getElementById('speeking');
+		var parallax = new Parallax(scene);
 	},
 	methods: {
 		isActive: function(index) {
